1e693f009e84737c6afff32310ca82d599048042
Maksim Korzh <freesoft.for.people@gmail.com>
2021-12-08 12:11:56 +0200

    added alphabet table

---
+++ software/KIM-Alphabet/alphabet.asm
@@ -0,0 +1,46 @@
+;
+; Lookup table of 7 segment codes representing digits & letters
+;                     by Dominic Bumbaca
+;
+
+define sad  $1740  ; A data register
+define padd $1741  ; A data direction register
+define sbd  $1742  ; B data register
+define pbdd $1743  ; B data direction register
+
+;1 $06
+;2 $5b
+;3 $4f
+;4 $66
+;5 $6d
+;6 $7c
+;7 $07
+;8 $7f
+;9 $6f
+;0 $3f
+;a $57
+;b $7c
+;c $39
+;d $5e
+;e $79
+;f $71
+;g $3d
+;h $f6
+;i $10
+;j $1e
+;k $75
+;l $30
+;m $37
+;n $54
+;o $3f
+;p $73
+;q $67
+;r $50
+;s $6d
+;t $46
+;u $3e
+;v $1c
+;w $36
+;x $64
+;y $66
+;z $52
